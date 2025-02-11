f9e86cd5dea16339c2e1e421bfe0a7e962d1ab70
leninfontella <lenin_fontella@hotmail.com>
2025-02-11 18:58:46 -0300

    format

--- script.js
+++ script.js
@@ -1,87 +1,76 @@
-const menu = document.getElementById("menu")
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("cart-modal")
-const cartItemsContainer = document.getElementById("cart-items")
-const cartTotal = document.getElementById("cart-total")
-const checkoutBtn = document.getElementById("checkout-btn")
-const closeModalBtn = document.getElementById("close-modal-btn")
-const cartCounter = document.getElementById("cart-count")
-const addressInput = document.getElementById("address")
-const addressWarn = document.getElementById("address-warn")
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
 
+let cart = [];
 
-let cart = []
+cartBtn.addEventListener("click", function () {
+  cartModal.style.display = "flex";
 
-cartBtn.addEventListener("click", function() {
+  updateCartMordel();
+});
 
-cartModal.style.display = "flex"
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
 
-updateCartMordel();
+  console.log(event);
+});
 
-})
+closeModalBtn.addEventListener("click", function (event) {
+  cartModal.style.display = "none";
+});
 
-cartModal.addEventListener("click", function(event) {
-if(event.target === cartModal) {
-cartModal.style.display = "none"
-}
+menu.addEventListener("click", function (event) {
+  let parentButton = event.target.closest(".add-to-cart-btn");
 
-console.log(event);
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
 
-})
-
-closeModalBtn.addEventListener("click", function(event) {
-cartModal.style.display = "none"
-})
-
-menu.addEventListener("click", function(event){
-
-let parentButton = event.target.closest(".add-to-cart-btn")
-
-if(parentButton){
-
-const name = parentButton.getAttribute("data-name")
-const price = parseFloat(parentButton.getAttribute("data-price"))
-
-addToCart(name, price)
-
-}
-} )
+    addToCart(name, price);
+  }
+});
 
 function addToCart(name, price) {
-const existingItem = cart.find(item => item.name === name)
+  const existingItem = cart.find((item) => item.name === name);
 
-if(existingItem) {
-existingItem.quantity += 1;
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
 
-}
-
-else {
-cart.push (
-    {
-    name,
-    price,
-    quantity: 1,
-})
-
-
-}
-
-updateCartMordel()
-
+  updateCartMordel();
 }
 
 function updateCartMordel() {
+  cartItemsContainer.innerHTML = "";
 
-cartItemsContainer.innerHTML = "";
+  let total = 0;
 
-let total = 0;
+  cart.forEach((item) => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add(
+      "flex",
+      "justify-between",
+      "mb-4",
+      "flex-col"
+    );
 
-cart.forEach(item => {
-
-const cartItemElement = document.createElement ("div");
-cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
-
-cartItemElement.innerHTML = `
+    cartItemElement.innerHTML = `
 
 <div class="flex items-center justify-between">
 
@@ -103,139 +92,115 @@ Remover
 </div>
 
 
-`
+`;
 
-total += item.price * item.quantity;
+    total += item.price * item.quantity;
 
-cartItemsContainer.appendChild(cartItemElement)
+    cartItemsContainer.appendChild(cartItemElement);
+  });
 
-})
+  cartTotal.textContent = total.toLocaleString(
+    "pt-br",
 
-cartTotal.textContent = total.toLocaleString("pt-br" , 
+    {
+      style: "currency",
+      currency: "BRL",
+    }
+  );
 
+  cartCounter.innerText = cart.length;
+}
 
-{
+cartItemsContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name");
 
-    style: "currency" ,
-    currency: "BRL" 
+    removeItemCart(name);
+  }
 });
 
+function removeItemCart(name) {
+  const index = cart.findIndex((item) => item.name === name);
 
-cartCounter.innerText = cart.length;
-
-}
-
-
-cartItemsContainer.addEventListener("click", function (event){
-
-if(event.target.classList.contains("remove-from-cart-btn")){
-
-const name = event.target.getAttribute("data-name")
-
-removeItemCart(name);
-}
-
-})
-
-function removeItemCart(name){
-const index = cart.findIndex(item => item.name === name);
-
-if(index !== -1) 
-{
+  if (index !== -1) {
     const item = cart[index];
 
-    if(item.quantity > 1){
-        item.quantity -= 1;
-        updateCartMordel();
-        return;
-
-
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartMordel();
+      return;
     }
 
     cart.splice(index, 1);
     updateCartMordel();
-
-
-}
+  }
 }
 
-addressInput.addEventListener("input" , function(event){
+addressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
 
-let inputValue = event.target.value;
-if(inputValue !== ""){
-
-    addressInput.classList.remove("border-red-500")
-    addressWarn.classList.add("hidden")
-
-}
-
-})
-
-checkoutBtn.addEventListener("click" , function(){
-
-const isOpen = checkRestaurantOpen();
-if(!isOpen){
-
-Toastify({
-
-    text: "Lênin's Burguer fechado no momento",
-    duration: 3000,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "#ef4444",
-    },
-    
-}).showToast();
+checkoutBtn.addEventListener("click", function () {
+  const isOpen = checkRestaurantOpen();
+  if (!isOpen) {
+    Toastify({
+      text: "Lênin's Burguer fechado no momento",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#ef4444",
+      },
+    }).showToast();
 
     return;
+  }
+
+  if (cart.length === 0) return;
+
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+    return;
+  }
+
+  const cartItems = cart
+    .map((item) => {
+      return ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}`;
+    })
+    .join("");
+
+  const message = encodeURIComponent(cartItems);
+  const phone = "5551989134037";
+
+  window.open(
+    `https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`,
+    "_blank"
+  );
+
+  cart = [];
+  updateCartMordel();
+});
+
+function checkRestaurantOpen() {
+  const data = new Date();
+  const hora = data.getHours();
+  return hora >= 18 && hora < 22;
 }
 
-if(cart.length === 0) return;
-
-if(addressInput.value ===""){
-addressWarn.classList.remove("hidden")
-addressInput.classList.add("border-red-500")
-return;
-}
-
-const cartItems = cart.map( (item) => {
-return (
-    ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}`
-)
-
-}).join("")
-
-const message = encodeURIComponent(cartItems)
-const phone = "5551989134037"
-
-window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
-
-cart = [];
-updateCartMordel();
-
-})
-
-
-function checkRestaurantOpen(){
-
-const data = new Date();
-const hora = data.getHours();
-return hora >= 18 && hora < 22;
-
-}
-
-const spanItem = document.getElementById("date-span")
+const spanItem = document.getElementById("date-span");
 const isOpen = checkRestaurantOpen();
 
-if(isOpen){
-spanItem.classList.remove("bg-red-500");
-spanItem.classList.add("bg-green-600")
-
-}else{
-spanItem.classList.remove("bg-green-600");
-spanItem.classList.add("bg-red-500")
-
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else {
+  spanItem.classList.remove("bg-green-600");
+  spanItem.classList.add("bg-red-500");
 }
